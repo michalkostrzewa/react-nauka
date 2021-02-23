8df714b8b0c3befcff30941f2d4325dbfb7df4ba
michalkostrzewa <michallgk10@gmail.com>
2021-02-23 14:31:33 +0100

    created basic structure

---
+++ noclegi/src/components/Hotels/Hotels.js
@@ -0,0 +1,15 @@
+import React, { Component }  from 'react';
+import Hotel from './Hotel/Hotel'
+
+class Hotels extends React.Component{
+    render() {
+        return (
+            <div>
+                Hotels
+                <Hotel/>
+            </div>
+        );
+    }
+} 
+
+export default Hotels;
